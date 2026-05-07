5f50a84e99f2742fe0bdd5a63bbe3b23ac514fc7
RoRep1ay <sojimmynea@gmail.com>
2026-05-07 14:00:24 +0700

    fix deploy script

--- frontend/deploy-frontend.sh
+++ frontend/deploy-frontend.sh
@@ -2,16 +2,12 @@
 set -euo pipefail
 
 FRONTEND_DIR="/var/www/takehome-assignment/frontend"
-NGINX_DIR="/var/www/takehome-assignment/frontend/dist"
 
 cd "$FRONTEND_DIR"
 
 npm install
 npm run build
 
-sudo mkdir -p "$NGINX_DIR"
-sudo rm -rf "$NGINX_DIR"/*
-sudo cp -r dist/* "$NGINX_DIR"/
 sudo chmod -R o+rx /var/www/takehome-assignment
 
 sudo nginx -t
