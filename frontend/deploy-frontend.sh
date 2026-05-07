31239ab8a479d2ecdb32bf3ee2bdfaf619d7c922
RoRep1ay <sojimmynea@gmail.com>
2026-05-07 14:06:23 +0700

    revert

--- frontend/deploy-frontend.sh
+++ frontend/deploy-frontend.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-FRONTEND_DIR="/var/www/takehome-assignment/frontend"
+FRONTEND_DIR="$HOME/takehome-assignment/frontend"
+NGINX_DIR="/var/www/takehome-assignment/frontend/dist"
 
 cd "$FRONTEND_DIR"
 
 npm install
 npm run build
 
+sudo mkdir -p "$NGINX_DIR"
+sudo rm -rf "$NGINX_DIR"/*
+sudo cp -r dist/* "$NGINX_DIR"/
 sudo chmod -R o+rx /var/www/takehome-assignment
 
 sudo nginx -t
