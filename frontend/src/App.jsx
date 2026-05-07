6872a0297f66abcf8a4723405b7b1fd5e46e8170
RoRep1ay <sojimmynea@gmail.com>
2026-05-07 12:35:39 +0700

    remove dummy page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import './App.css'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import { Login } from './pages/Login'
 import { Products } from './pages/Products'
-import { DummyPage } from './pages/DummyPage'
 import { ProtectedRoute } from './components/ProtectedRoute'
 import { MainLayout } from './components/MainLayout'
 import './i18n'
@@ -11,30 +10,6 @@ function App() {
   return (
     <div className="app-container">
       <Routes>
-        <Route
-          path="/"
-          element={
-            <MainLayout>
-              <DummyPage title="Home" />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/order"
-          element={
-            <MainLayout>
-              <DummyPage title="Order" />
-            </MainLayout>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <MainLayout>
-              <DummyPage title="About Us" />
-            </MainLayout>
-          }
-        />
         <Route
           path="/login"
           element={
--- frontend/src/pages/DummyPage.jsx
+++
@@ -1,8 +0,0 @@
-export const DummyPage = ({ title }) => {
-  return (
-    <main>
-      <h1>{title}</h1>
-      <p>This is a placeholder page.</p>
-    </main>
-  )
-}
